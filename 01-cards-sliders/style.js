4dcdd068a8dccc58a643c31e0760b78537a5b350
Oleksandr <opetrovskyi423@gmail.com>
2021-10-02 10:19:56 +0300

    slider_upgrade

--- 01-cards-sliders/style.js
+++ 01-cards-sliders/style.js
@@ -1,17 +1,22 @@
-const sliders = document.querySelectorAll('.slide')
-const container = document.querySelectorAll('.container')
 
-sliders.forEach((item)=>{
-  item.addEventListener('click',()=>{
-    removeActiveClass()
-    item.classList.add('active')
-  })
-})
 
-function removeActiveClass(){
+
+function slider(count) {
+  const sliders = document.querySelectorAll('.slide')
+  sliders[count].classList.add('active')
   sliders.forEach((item)=>{
-  
-      item.classList.remove('active')
-    
+    item.addEventListener('click',()=>{
+      removeActiveClass()
+      item.classList.add('active')
+    })
   })
+  
+  function removeActiveClass(){
+    sliders.forEach((item)=>{
+      
+      item.classList.remove('active')
+      
+    })
+  }
 }
+slider(1)
