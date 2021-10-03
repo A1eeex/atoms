07cd6eeafe138e5d82029d3d9d69db18e4964e95
Oleksandr <opetrovskyi423@gmail.com>
2021-10-03 17:06:24 +0300

    03_slider

---
+++ 03-slider-sources (1)/script.js
@@ -0,0 +1,39 @@
+const downButton = document.querySelector('.down-button')
+const upButton = document.querySelector('.up-button')
+const sidebar = document.querySelector('.sidebar')
+const container = document.querySelector('.container')
+const mainSlide = document.querySelector('.main-slide')
+
+const slideCount = mainSlide.querySelectorAll('div').length
+
+let activeIndex = 0
+
+sidebar.style.top = `-${(slideCount - 1) * 100}vh`
+
+upButton.addEventListener('click', () => {
+  changeSlide('up')
+})
+
+downButton.addEventListener('click', () => {
+  changeSlide('down')
+})
+
+
+function changeSlide(direction) {
+  if (direction === 'up') {
+    activeIndex++
+    if (activeIndex === slideCount) {
+      activeIndex = 0
+    }
+  } else if (direction === 'down') {
+    activeIndex--
+    if (activeIndex < 0) {
+      activeIndex = slideCount - 1
+    }
+  }
+
+const height = container.clientHeight
+
+mainSlide.style.transform = `translateY(-${activeIndex * height}px)`
+sidebar.style.transform = `translateY(${activeIndex * height}px)`
+}
